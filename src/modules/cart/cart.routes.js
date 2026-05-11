773702cf6ecc9897e95faa00336145cc81f9ea70
Deepak Rai <dpk.developer04@gmail.com>
2026-05-11 18:00:42 +0530

    new chnages

--- src/modules/cart/cart.routes.js
+++ src/modules/cart/cart.routes.js
@@ -8,15 +8,16 @@ import {
 } from './cart.controller.js';
 import { authMiddleware } from '../../middlewares/authMiddleware.js';
 import { optionalAuth } from '../../middlewares/optionalauth.js';
+import { asyncHandler } from '../../utils/asyncHandler.js';
 
 
 
 const cartRouter = Router();
 
-cartRouter.get('/', optionalAuth, getCart);
-cartRouter.post('/add', optionalAuth, addToCart);
-cartRouter.put('/update', authMiddleware, updateCart);
-cartRouter.delete('/delete/:cart_id', optionalAuth, removeFromCart);
-cartRouter.delete('/clear', authMiddleware, clearCart);
+cartRouter.get('/', optionalAuth, asyncHandler(getCart));
+cartRouter.post('/add', optionalAuth, asyncHandler(addToCart));
+cartRouter.put('/update', authMiddleware, asyncHandler(updateCart));
+cartRouter.delete('/delete/:cart_id', optionalAuth, asyncHandler(removeFromCart));
+cartRouter.delete('/clear', authMiddleware, asyncHandler(clearCart));
 
 export default cartRouter;
